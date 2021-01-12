0c4af6b2e5e96f6e9fbdab93a655f0570b3b4f08
BuildTools <37029865+bjrk99@users.noreply.github.com>
2021-01-12 14:27:43 +0000

    Difficulty increases as game goes on, needs refining

--- js/main.js
+++ js/main.js
@@ -58,8 +58,15 @@ function spawnWave() {
 }
 
 function spawnEnemies() {
-    spawnWave()
-    setInterval(spawnWave, 4000)
+    let interval = 5000
+    timer = () => {
+        if (interval > 1000) {
+            interval -= 200
+        }
+        spawnWave()
+        setTimeout(timer, interval)
+    }
+    timer()
 }
 
 spawnEnemies()
