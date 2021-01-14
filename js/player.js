899dc9fc1db7380682bf75e52773c86cf747b86d
BuildTools <37029865+bjrk99@users.noreply.github.com>
2021-01-14 18:14:02 +0000

    Tidy up player class

--- js/player.js
+++ js/player.js
@@ -2,45 +2,31 @@ class Player {
     constructor(){
         this.x = innerWidth / 2;
         this.y = innerHeight - 100;
-        // this.x = canvas.width / 2;
-        // this.y = canvas.height - 100;
+        
         this.width = 20;
         this.height = 20;
-        this.score = 0;
-        this.fuel = 2000
 
-        console.log(innerHeight, innerWidth)
         this.widthSpeedFactor = innerWidth / 140
         this.heightSpeedFactor = innerHeight / 80
+
+        this.score = 0;
+        this.fuel = 2000
     }
+
     draw(){
         ctx.fillStyle = 'white';
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+
     drawFuelGauge(){
         ctx.fillStyle = 'green'
         ctx.fillRect(innerWidth - 50, innerHeight - (this.fuel / 10), 50, this.fuel / 10)
     }
-    moveLeft(){
-        if (this.x > 8){
-            this.x -= this.widthSpeedFactor;
-        }
+
+    addScore(score){
+        this.score += score;
     }
-    moveRight(){
-        if (this.x < (canvas.width - 28)){
-            this.x += this.widthSpeedFactor;
-        }
-    }
-    moveUp(){
-        if (this.y > 8){
-            this.y -= this.heightSpeedFactor;
-        }
-    }
-    moveDown(){
-        if (this.y < (canvas.height - 28)){
-            this.y += this.heightSpeedFactor;
-        }
-    }
+
     distanceTo(obj) {
         if (obj.radius == null){
             obj = {
@@ -74,11 +60,28 @@ class Player {
         }
     }
 
-    addScoreForAvoidedObjects(){
-        this.score += 5;
+    moveLeft(){
+        if (this.x > 8){
+            this.x -= this.widthSpeedFactor;
+        }
     }
-    addScoreForCollectable(){
-        this.score += 25
+
+    moveRight(){
+        if (this.x < (canvas.width - 28)){
+            this.x += this.widthSpeedFactor;
+        }
+    }
+
+    moveUp(){
+        if (this.y > 8){
+            this.y -= this.heightSpeedFactor;
+        }
+    }
+
+    moveDown(){
+        if (this.y < (canvas.height - 28)){
+            this.y += this.heightSpeedFactor;
+        }
     }
 }
 
