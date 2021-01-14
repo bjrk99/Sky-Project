5a948d809947605fa53c0ef64c4b9672137a6400
BuildTools <37029865+bjrk99@users.noreply.github.com>
2021-01-14 19:51:31 +0000

    No reason for random y height when on random timer

--- js/fuel.js
+++ js/fuel.js
@@ -42,7 +42,7 @@ class Fuel {
     randSpawnPoint() {
         let sp = {
             x: Math.random() * (innerWidth - 100) + 50,
-            y: 0 - this.height - (Math.random() * 250)
+            y: 0 - this.height
         }
         console.log(sp)
         return sp
