5f80306cec3b84d093c5ff6562dcc6de35b1e814
GirtsJermacans <girts.jermacans@gmail.com>
2021-02-15 14:51:39 +0000

    improved api

--- js/api.js
+++ js/api.js
@@ -1,3 +1,4 @@
+let previousState = false;
 
 // load event - checks online property of navigator
 window.addEventListener("load", (event) => {
@@ -7,12 +8,11 @@ window.addEventListener("load", (event) => {
         mainGamePlayScreen = false;
         gameOverScreen = false;
     }
-  });
+  })
 
   // offline event listener
   window.addEventListener("offline", (event) => {
     startingScreen = true;
-    console.log(navigator.onLine);
   });
 
   // online event listener
@@ -20,6 +20,45 @@ window.addEventListener("load", (event) => {
     startingScreen = false;
     mainGamePlayScreen = false;
     gameOverScreen = false;
-    console.log(navigator.onLine);
+  });
+
+  // checks online status - returns true or false
+  const checkOnlineStatus = async () => {
+    try {
+      const online = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+      return online.status >= 200 && online.status < 300; // either true or false
+    } catch (err) {
+      return false; // definitely offline
+    }
+  };
+
+  // every 3 seconds check the status of online
+  setInterval(async () => {
+    const result = await checkOnlineStatus();
+    if (previousState != result){
+        if (result){
+            startingScreen = false;
+            mainGamePlayScreen = false;
+            gameOverScreen = false;
+            previousState = result;
+        }
+        else{
+            startingScreen = true;
+            mainGamePlayScreen = false;
+            gameOverScreen = false;
+            previousState = result;
+        }   
+      }
+  }, 3000);
+
+  window.addEventListener("load", async (event) => {
+    const statusOnline = (await checkOnlineStatus());
+
+      if (statusOnline){
+        startingScreen = false;
+        mainGamePlayScreen = false;
+        gameOverScreen = false;
+    }
+
   });
 
