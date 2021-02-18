3e80a40757a29487f513fc9907be490dcdeeebab
GirtsJermacans <girts.jermacans@gmail.com>
2021-02-18 15:09:15 +0000

    cleaned up Holding page css

--- js/holdingPage.js
+++
@@ -1,8 +0,0 @@
-
-
-function returnToGame(){
-    window.location.href = ("index.html");
-    startingScreen = true;
-}
-
-  document.getElementById("keepPlaying").addEventListener("click", returnToGame);
