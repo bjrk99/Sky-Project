ddfb869e64eeb3386b19dcef5545d415b2c2b65e
GirtsJermacans <girts.jermacans@gmail.com>
2021-02-18 15:50:28 +0000

    added try catch for api script

--- js/api.js
+++ js/api.js
@@ -24,28 +24,41 @@ function loadGamePage(){
 
   // method gets online status
   const getOnlineStatus = async () => {
-      let uri = "http://localhost:3000/serverStatus";
+      
+    let uri = "http://localhost:3000/serverStatus";
 
+    try {
       const res = await fetch(uri);
       const serverStatus = await res.json();
 
       const status = serverStatus.online;
       return status;
+
+    } catch (err) {
+        console.log("JSON-SERVER IS NOT WATCHING DB.JSON FILE");
+    }
   };
 
  // every 3 seconds gets online status
   setInterval(async () => {
-    const result = await getOnlineStatus();
-    if (result){
-      if (document.title == "Game"){
-        window.location.href = ("holdingPage.html");
+
+    try {
+
+      const result = await getOnlineStatus();
+      
+      if (result){
+        if (document.title == "Game"){
+          window.location.href = ("holdingPage.html");
+        }
       }
-    }
-    else {
-      if (document.title == "Back Online"){
-        window.location.href = ("index.html");
+      else {
+        if (document.title == "Back Online"){
+          window.location.href = ("index.html");
+        }
       }
+
+    } catch (err) {
+        console.log("JSON-SERVER IS NOT WATCHING DB.JSON FILE");
     }
-    console.log(result);
   }, 3000);
 
